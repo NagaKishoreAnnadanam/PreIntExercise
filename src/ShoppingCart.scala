2350057c6451c71b727ff9cdcfb33b89df3e6e01
NagaKishoreAnnadanam <naga.annadanam@gmail.com>
2017-09-19 23:13:01 +0100

    Commit v0.2
    changed the ShoppingCart calss to include offers for Apples and Oranges

--- src/ShoppingCart.scala
+++ src/ShoppingCart.scala
@@ -8,7 +8,17 @@ class ShoppingCart (val listOfItems: List[String]) {
   def cartCheckout(): Double = {
     var totalPrice: Double = 0.0;
     val itemMap = listOfItems.groupBy(identity).mapValues(_.size);
-     totalPrice = itemMap("Apple") * applePrice + itemMap("Orange") * orangePrice
-    totalPrice 
+     totalPrice = (java.lang.Math.floor(itemMap("Apple")/2) * applePrice) + 
+     ((itemMap("Apple")%2)*applePrice) +
+     (java.lang.Math.floor(itemMap("Orange")/3) * orangePrice * 2) + 
+     ((itemMap("Orange")%3)*orangePrice);
+     totalPrice 
   }
+}
+
+object Main extends App {
+   new ShoppingCart(List("Apple","Orange","Apple","Orange","Apple","Orange","Apple","Orange","Apple","Orange","Apple","Orange","Apple","Orange","Banana","Banana","Banana","Pineapple","Pineapple","Pineapple")){
+     //print(listOfItems.filter(x => (x!="Apple")).filter(x => (x!="Orange")));
+     print(cartCheckout)
+   };
 }
